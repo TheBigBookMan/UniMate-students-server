33e6f581b9f05b18bb686a52f0af28ac8ea4f4b6
Ben Smerd <bsmerd@adam.com.au>
2024-06-25 21:54:31 +0930

    add in Student model

---
+++ UniMate-students-server/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniMate_students_server.Models
+{
+    public class Student
+    {
+        [Key]
+        public int studentid { get; }
+        public string username { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }   
+        public string uniemail { get; set; }
+        public string unistudentid { get; }
+        public string email { get; set; }
+        public DateOnly dob { get; set; }
+        public DateTime createdat { get; set; }
+    }
+}
