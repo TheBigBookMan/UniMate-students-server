ff8e036848d61b3512dc174ad5e27331193b660f
Ben Smerd <bsmerd@adam.com.au>
2024-06-28 21:11:01 +0930

    create auth model

---
+++ UniMate-students-server/Models/Auth.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UniMate_students_server.Models
+{
+    public class Auth
+    {
+        [Key]
+        [Column("authid")]
+        public int AuthId { get; set; }
+
+        [Column("studentid")]
+        public int StudentId { get; set; }
+
+        [ForeignKey("studentid")]
+        public Student Student { get; set; }
+
+        [Column("passwordhash")]
+        public string PasswordHash { get; set; }
+
+        [Column("passwordsalt")]
+        public string PasswordSalt { get; set; }
+
+        [Column("createdat")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedAt { get; set; }
+    }
+}
