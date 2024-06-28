7d92cbe22ec85c42dfef40db8664fbbaa745ccff
Ben Smerd <bsmerd@adam.com.au>
2024-06-29 08:50:30 +0930

    add in a PasswordHelper class

---
+++ UniMate-students-server/Helpers/PasswordHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace UniMate_students_server.Helpers
+{
+    public class PasswordHelper
+    {
+        public static string CreatePasswordHash(string password, out string salt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                salt = Convert.ToBase64String(hmac.Key);
+                var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return Convert.ToBase64String(hash);
+            }
+        }
+    }
+}
