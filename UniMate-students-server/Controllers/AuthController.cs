e50966e21f7d72e69bb0a5a9824f2e00e553baf4
Ben Smerd <bsmerd@adam.com.au>
2024-06-28 21:54:21 +0930

    add in the login in authController

---
+++ UniMate-students-server/Controllers/AuthController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniMate_students_server.Contexts;
+using Microsoft.EntityFrameworkCore;
+using UniMate_students_server.Models;
+using Microsoft.AspNetCore.Identity.Data;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace UniMate_students_server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AuthController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        private UniversityContext GetDynamicDbContext()
+        {
+            return _httpContextAccessor.HttpContext.Items["DynamicDbContext"] as UniversityContext;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            var dbContext = GetDynamicDbContext();
+            if (dbContext == null)
+            {
+                return BadRequest("Database context not found.");
+            }
+            
+            var authRecord = await dbContext.Auths.FirstOrDefaultAsync(a => a.StudentId == request.StudentId);
+
+            if (authRecord == null)
+            {
+                return Unauthorized("Inavlid credentials");
+            }
+
+            if(!VerifyPasswordHash(request.Password, authRecord.PasswordHash, authRecord.PasswordSalt))
+            {
+                return Unauthorized("Invalid credentials");
+            }
+
+            return Ok("Login successful.");
+        }
+
+        private bool VerifyPasswordHash(string password, string storedHash, string storedSalt)
+        {
+            using (var hmac = new HMACSHA512(Encoding.UTF8.GetBytes(storedSalt)))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return storedHash == Convert.ToBase64String(computedHash);
+            }
+        }
+
+        public class LoginRequest
+        {
+            public int StudentId { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}
