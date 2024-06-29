d02b4348d1346c498030f7faeab63200a3b20ff1
Ben Smerd <bsmerd@adam.com.au>
2024-06-30 09:15:09 +0930

    add in BaseResponse DTO in AuthDTO

---
+++ UniMate-students-server/DTOs/AuthDTO/BaseResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace UniMate_students_server.DTOs.AuthDTO
+{
+    public class BaseResponse
+    {
+        public int StudentId { get; set; }
+        public string ResponseMessage { get; set; }
+    }
+}
