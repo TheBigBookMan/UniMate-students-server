b5049e3a4934d6ebce620e167e30af02636492d2
Ben Smerd <bsmerd@adam.com.au>
2024-06-30 19:56:30 +0930

    add a DegreesController

---
+++ UniMate-students-server/Controllers/DegreesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniMate_students_server.Contexts;
+using Microsoft.EntityFrameworkCore;
+using UniMate_students_server.Models;
+
+namespace UniMate_students_server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DegreesController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public DegreesController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        private UniversityContext GetDynamicDbContext()
+        {
+            return _httpContextAccessor.HttpContext.Items["DynamicDbContext"] as UniversityContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddDegree([FromBody])
+    }
+}
