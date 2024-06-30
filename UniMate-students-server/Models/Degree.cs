f3f6f1ae178d5345d86ae9ce717214317ba84cfb
Ben Smerd <bsmerd@adam.com.au>
2024-06-30 20:05:09 +0930

    add Degree model

---
+++ UniMate-students-server/Models/Degree.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UniMate_students_server.Models
+{
+    public class Degree
+    {
+        [Key]
+        public int DegreeId { get; set; }
+
+        [Column("degreename")]
+        public string DegreeName { get; set; }
+
+        [Column("degreecode")]
+        public string DegreeCode { get; set; }
+
+        [Column("description")]
+        public string Description { get; set; }
+
+        [Column("department")]
+        public string Department { get; set; }
+
+        [Column("createdat")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedAt { get; set; }
+    }
+}
