012d094bcb6ea10bc0810cc8dd8719e6b1731ec0
Ben Smerd <bsmerd@adam.com.au>
2024-06-30 18:41:00 +0930

    add in the ProfileController and getProfile endpoint

---
+++ UniMate-students-server/Controllers/ProfileController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniMate_students_server.Contexts;
+using UniMate_students_server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace UniMate_students_server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProfileController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ProfileController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        private UniversityContext GetDynamicDbContext()
+        {
+            return _httpContextAccessor.HttpContext.Items["DynamicDbContext"] as UniversityContext;
+        }
+
+        [HttpGet("{studentId}")]
+        public async Task<IActionResult> GetProfile(int StudentId)
+        {
+            var dbContext = GetDynamicDbContext();
+            if(dbContext == null)
+            {
+                return BadRequest("Database context not found.");
+            }
+
+            var student = await dbContext.Students.FindAsync(StudentId);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(student);
+        }
+    }
+}
