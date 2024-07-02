6d5cb8f536279df0177c392d83b7bc7f715cd4fc
Ben Smerd <bsmerd@adam.com.au>
2024-07-02 19:10:25 +0930

    create UniversityController

---
+++ UniMate-students-server/Controllers/UniversityController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniMate_students_server.Contexts;
+using Microsoft.EntityFrameworkCore;
+using UniMate_students_server.Models;
+using UniMate_students_server.Helpers;
+
+namespace UniMate_students_server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UniversityController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UniversityController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        private DynamicDbContext GetDynamicDbContext()
+        {
+            return _httpContextAccessor.HttpContext.Items["DynamicDbContext"] as DynamicDbContext;
+        }
+
+    }
+}
